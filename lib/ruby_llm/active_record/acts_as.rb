287c1bb6876186e2a48cebad1e2cd114ceba0d2c
Jay El-Kaake <najibkaake@gmail.com>
2025-04-21 17:42:22 -0400

    Implement working with_response_format for Open AI

--- lib/ruby_llm/active_record/acts_as.rb
+++ lib/ruby_llm/active_record/acts_as.rb
@@ -104,6 +104,12 @@ module RubyLLM
         self
       end
 
+      # @see LlmChat#with_response_format
+      def with_response_format(...)
+        to_llm.with_response_format(...)
+        self
+      end
+
       def with_tool(...)
         to_llm.with_tool(...)
         self
@@ -182,6 +188,7 @@ module RubyLLM
             output_tokens: message.output_tokens
           )
           @message.write_attribute(@message.class.tool_call_foreign_key, tool_call_id) if tool_call_id
+          @message.try('content_schema=', message.content_schema)
           @message.save!
           persist_tool_calls(message.tool_calls) if message.tool_calls.present?
         end
