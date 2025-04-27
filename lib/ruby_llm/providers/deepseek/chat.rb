b43eb14aaec1b300e15f12c90e2e54a4c3ec5502
Eric-Guo <eric.guocz@gmail.com>
2025-04-27 11:00:05 +0800

    Add deepseek support.

--- lib/ruby_llm/providers/deepseek/chat.rb
+++ lib/ruby_llm/providers/deepseek/chat.rb
@@ -11,6 +11,23 @@ module RubyLLM
           # DeepSeek doesn't use the new OpenAI convention for system prompts
           role.to_s
         end
+
+        def render_payload(messages, tools:, temperature:, model:, stream: false)
+          {
+            model: model,
+            messages: format_messages(messages),
+            temperature: temperature,
+            stream: stream
+          }.tap do |payload|
+            add_response_schema_to_payload(payload) if response_schema.present?
+          end
+        end
+
+        private
+
+        def add_response_schema_to_payload(payload)
+          payload[:response_format] = { type: 'json_object' }
+        end
       end
     end
   end
