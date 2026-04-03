6992c0dad8665d495e2fcafe8b9a816da418aa91
Eric-Guo <eric.guocz@gmail.com>
2026-04-03 13:00:17 +0800

    Add file support

--- lib/ruby_llm/attachment.rb
+++ lib/ruby_llm/attachment.rb
@@ -30,6 +30,8 @@ module RubyLLM
     # The detected MIME type string, such as <tt>"image/png"</tt>.
     attr_reader :mime_type
 
+    attr_reader :upload_file_id # :nodoc:
+
     # File extensions recognized as document attachments when the MIME type
     # alone is inconclusive.
     DOCUMENT_EXTENSIONS = %w[
@@ -82,7 +84,7 @@ module RubyLLM
       @source = source_type_cast
       @filename = filename || source_filename
 
-      determine_mime_type
+      determine_mime_type if @upload_file_id.nil?
     end
 
     def config # :nodoc:
@@ -113,7 +115,12 @@ module RubyLLM
     end
 
     def path? # :nodoc:
-      !provider_file? && (@source.is_a?(Pathname) || (@source.is_a?(String) && !url?))
+      return false if provider_file?
+      return true if @source.is_a?(Pathname)
+      return false unless @source.is_a?(String)
+      return false if url? || uuid?
+
+      true
     end
 
     def io_like? # :nodoc:
@@ -122,6 +129,12 @@ module RubyLLM
 
     def active_storage? # :nodoc:
       ACTIVE_STORAGE_CLASS_NAMES.any? { |class_name| source_is_a?(class_name) }
+    end
+
+    def uuid? # :nodoc:
+      return false unless @source.is_a?(String)
+
+      @source.match?(/\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/i)
     end
 
     # Returns the raw bytes of the attachment, fetching or reading the source
@@ -155,6 +168,7 @@ module RubyLLM
     # Returns the attachment category as a Symbol: +:image+, +:video+,
     # +:audio+, +:pdf+, +:text+, +:document+, or +:unknown+.
     def type
+      return :file_id unless @upload_file_id.nil?
       return :image if image?
       return :video if video?
       return :audio if audio?
@@ -309,6 +323,8 @@ module RubyLLM
     def source_type_cast
       if url?
         URI(@source)
+      elsif uuid?
+        @upload_file_id = @source
       elsif path?
         Pathname.new(@source)
       else
@@ -317,6 +333,8 @@ module RubyLLM
     end
 
     def source_filename
+      return 'attachment' unless @upload_file_id.nil?
+
       if url?
         File.basename(@source.path).to_s
       elsif provider_file?
